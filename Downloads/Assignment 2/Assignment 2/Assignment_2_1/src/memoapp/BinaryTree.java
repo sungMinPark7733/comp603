818468508edadafb2eed1f1cbef5a8ab200d1a51
smoothpeanutbutter <park64906@gmail.com>
2023-05-03 12:21:18 +1200

    first commit default files

---
+++ Downloads/Assignment 2/Assignment 2/Assignment_2_1/src/memoapp/BinaryTree.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memoapp;
+
+/**
+ *
+ * @author xhu
+ */
+public class BinaryTree <E, F extends Comparable> {
+
+    
+    public BinaryTree(Node node)
+    {
+        
+    }
+    
+    public BinaryTree(E element, F key)
+    {
+        
+    }
+    
+    public BinaryTree()
+    {
+        
+    }
+    
+    public void addElement(E element, F key)
+    {
+        
+    }
+    
+    public void addNode(Node root, Node node)
+    {        
+        
+    }
+    
+    public void traversal(Node root)
+    {
+        
+    }
+    
+    public Node[] toSortedList()
+    {
+        
+    }
+    
+    private void toSortedList(Node root)
+    {
+        
+    }
+    
+    public E searchElement(F key)
+    {
+        
+    }
+       
+    public Node searchNode(Node root, Node node)
+    {
+        
+    }
+    
+    public void reverseOrder()
+    {
+        
+    }
+    
+    private void reverseOrder(Node root)
+    {
+        
+    }
+    
+}
---
+++ Downloads/Assignment 2/Assignment 2/Assignment_2_1/src/memoapp/MemoManager.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memoapp;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author xhu
+ */
+public class MemoManager<E extends Comparable> {
+    
+    
+    
+    
+    public void addMemo(String date, String title, String message)
+    {
+        
+    }
+    
+    public void addToTree(Memo memo, E key)
+    {        
+               
+    }
+    
+    public Memo findMemo(E key)
+    {
+         
+    }
+    
+    public Memo[] getSortedMemoList(E key)
+    {
+        
+    }
+    
+    public void reverseOrder()
+    {
+        
+    }
+    
+}
